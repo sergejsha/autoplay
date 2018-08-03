11d873fb22763b6999d71386cc55410eb9361aba
Sergej Shafarenka <beworker@gmail.com>
2018-08-03 23:00:49 +0200

    Update Kotlin to 1.2.60

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,15 +17,15 @@ allprojects {
         all {
             resolutionStrategy {
                 force("com.google.guava:guava:22.0")
-                force("org.jetbrains.kotlin:kotlin-reflect:1.2.51")
+                force("org.jetbrains.kotlin:kotlin-reflect:1.2.60")
 
                 dependencySubstitution {
 
                     substitute(module("org.jetbrains.kotlin:kotlin-stdlib-jre8:1.2.0"))
-                        .with(module("org.jetbrains.kotlin:kotlin-stdlib:1.2.51"))
+                        .with(module("org.jetbrains.kotlin:kotlin-stdlib:1.2.60"))
 
                     substitute(module("org.jetbrains.kotlin:kotlin-stdlib-jre7:1.2.0"))
-                        .with(module("org.jetbrains.kotlin:kotlin-stdlib:1.2.51"))
+                        .with(module("org.jetbrains.kotlin:kotlin-stdlib:1.2.60"))
                 }
             }
         }
