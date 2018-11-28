d3db64a8401c8de12138df0f8c373ff9dd11fc87
Sergej Shafarenka <beworker@gmail.com>
2018-11-28 12:13:37 +0100

    Upgrading to Gradle 5.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,6 @@ buildscript {
         google()
         jcenter()
     }
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.2.61")
-    }
 }
 
 allprojects {
@@ -17,15 +14,6 @@ allprojects {
         all {
             resolutionStrategy {
                 force("com.google.guava:guava:22.0")
-                force("org.jetbrains.kotlin:kotlin-reflect:1.2.61")
-
-                dependencySubstitution {
-                    substitute(module("org.jetbrains.kotlin:kotlin-stdlib-jre8:1.2.0"))
-                        .with(module("org.jetbrains.kotlin:kotlin-stdlib:1.2.61"))
-
-                    substitute(module("org.jetbrains.kotlin:kotlin-stdlib-jre7:1.2.0"))
-                        .with(module("org.jetbrains.kotlin:kotlin-stdlib:1.2.61"))
-                }
             }
         }
     }
